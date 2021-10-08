b801c079caa3fb3a369dfb94ba28a8340a22c65d
yan-xi.zhu <547110356@qq.com>
2021-10-08 21:55:03 +0800

    Update strings.go
    string and byte array converting

--- strings.go
+++ strings.go
@@ -1,5 +1,10 @@
 package srutil
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 // DeduplicateArray : depulicate a slice
 func DeduplicateArray(s []string) []string {
 	res := NewSet(s...)
@@ -14,4 +19,19 @@ func ContainsString(array []string, val string) int {
 		}
 	}
 	return -1
+}
+
+// convert string to byte array without alloc
+func Str2Bytes(s string) (b []byte) {
+	h1 := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	h2 := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	h2.Cap = h1.Len
+	h2.Len = h1.Len
+	h2.Data = h1.Data
+	return
+}
+
+// convert byte array to string without alloc
+func Bytes2Str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
